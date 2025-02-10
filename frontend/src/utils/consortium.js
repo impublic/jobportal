f4b3557801d1b23823fd36532b905624c949214e
Navin chainani <47205958+impublic@users.noreply.github.com>
2025-02-10 20:35:11 +0530

    Update consortium.js

--- frontend/src/utils/consortium.js
+++ frontend/src/utils/consortium.js
@@ -1,4 +1,4 @@
-export const USER_API_Endpoint="http://localhost:4000/api/v1/user";
-export const JOB_API_END_POINT="http://localhost:4000/api/v1/job";
-export const APPLICATION_API_END_POINT  ="http://localhost:4000/api/v1/application";
-export const COMPANY_API_END_POINT  ="http://localhost:4000/api/v1/company";
+export const USER_API_Endpoint="https://jobportal-3ao1.onrender.com/api/v1/user";
+export const JOB_API_END_POINT="https://jobportal-3ao1.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT  ="https://jobportal-3ao1.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT  ="https://jobportal-3ao1.onrender.com/api/v1/company";
